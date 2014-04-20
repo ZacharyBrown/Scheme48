a72e681cc1241fad34cc43be81123b2d9278c99d
Zachary Brown <tzakyrie@gmail.com>
2014-04-20 03:45:35 -0400

    Incremental commit adding different radix parsing

--- src/Main.hs
+++ src/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import System.Environment
 import Text.ParserCombinators.Parsec hiding (spaces)
+import Numeric (readOct, readHex)
 
 main :: IO()
 main = do
@@ -25,7 +26,7 @@ data LispVal = Atom String
         
     
 symbol :: Parser Char
-symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+symbol = oneOf "!$%&|*+-/:<=>?@^_~"
 
 spaces :: Parser ()
 spaces = skipMany1 space
@@ -39,6 +40,7 @@ parseExpr :: Parser LispVal
 parseExpr = parseAtom 
         <|> parseString
         <|> parseNumber
+        <|> parseBool
     
               
 parseString :: Parser LispVal
@@ -51,7 +53,7 @@ parseString = do
 escapedChars :: Parser Char
 escapedChars = do
     _ <- char '\\'
-    c <- oneOf "\"\\nrt"
+    c <- oneOf ['\\', '"', 'n', 'r', 't']
     return $ case c of 
         '\\' -> c
         '"'  -> c
@@ -64,13 +66,59 @@ parseAtom = do
     first <- letter <|> symbol
     rest <- many (letter <|> digit <|> symbol)
     let atom = first:rest
-    return $ case atom of
-            "#t" -> Bool True
-            "#f" -> Bool False
-            _    -> Atom atom
+    return $ Atom atom
 
+parseBool :: Parser LispVal
+parseBool = do
+    _ <- char '#'
+    (char 't' >> return (Bool True))
+    <|> (char 'f' >> return (Bool False))
 
 parseNumber :: Parser LispVal
-parseNumber = many1 digit >>= return . Number . read
+parseNumber = parseDefaultDec <|> parseRadixDelimitedNum
 
+-- Parse 'normal' numbers
+parseDefaultDec :: Parser LispVal
+parseDefaultDec = many1 digit >>= return . Number . read 
+
+-- Parse numbers beginning with '#'
+parseRadixDelimitedNum :: Parser LispVal
+parseRadixDelimitedNum =
+    char '#'
+    >> (parseDec)
+
+-- Parse decimal numbers: beginning with "#d"
+parseDec :: Parser LispVal
+parseDec = do
+    _ <- char 'd'
+    num <- many1 digit
+    (return . Number . read ) num
+
+-- Parse binary numbers: beginning with "#b"
+
+-- binary parser helper
+-- needs to sum powers of 2 for each position in string that has a '1'
+binaryStr2Int :: String -> Integer
+binaryStr2Int s = 0
+
+-- Parse octal numbers: beginning with "#o"
+parseOctal :: Parser LispVal
+parseOctal = do
+    _ <- char 'o'
+    num <- many $ oneOf "01234567"
+    (return . Number . (readReadSWith readOct)) num
+
+-- Parse hexidecimal numbers: beginning with "#x"
+parseHex :: Parser LispVal
+parseHex = do
+    _ <- char 'x'
+    num <- many $ oneOf "0123456789abcdefABCDEF"
+    (return . Number . (readReadSWith readHex)) num
+
+-- Octal and Hex parser helper
+-- Both readOct and readHex return this ReadS type (first arg)
+-- So we need to unpack it into just the value we want...
+-- Assumes the first parse in the list from ReadS is good
+readReadSWith :: (String -> [(a, String)]) -> String -> a
+readReadSWith funct str = fst $ (funct str) !! 0
 
